09eff42d9dae8f65cc15d9d72d0ed7ad832679f6
tocomi <tocomi0112@gmail.com>
2022-10-12 02:31:47 +0900

    feat: add notion access

---
+++ src/domain/notion/notion.ts
@@ -0,0 +1,44 @@
+import { Client, isFullBlock, isFullPage } from '@notionhq/client'
+import {
+  BlockObjectResponse,
+  ListBlockChildrenResponse,
+  PageObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints'
+
+const notion = new Client({
+  auth: 'secret_FSLxNbd2Sn8D69Obym1YRv5bCHmgn4fZeOuJ1a8EuTs',
+})
+
+const DATABASE_ID = 'adc862a2f0484671a72451146842c838'
+
+export const getDatabase = async (databaseId = DATABASE_ID) => {
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  })
+  return response.results.filter<PageObjectResponse>((r): r is PageObjectResponse => isFullPage(r))
+}
+
+export const getPage = async (pageId: string) => {
+  const response = await notion.pages.retrieve({ page_id: pageId })
+  return response
+}
+
+export const getBlocks = async (blockId: string) => {
+  const blocks: BlockObjectResponse[] = []
+  let cursor
+  while (true) {
+    const response: ListBlockChildrenResponse = await notion.blocks.children.list({
+      start_cursor: cursor,
+      block_id: blockId,
+    })
+    const { results, next_cursor } = response
+    blocks.push(
+      ...results.filter<BlockObjectResponse>((r): r is BlockObjectResponse => isFullBlock(r))
+    )
+    if (!next_cursor) {
+      break
+    }
+    cursor = next_cursor
+  }
+  return blocks
+}
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,53 @@
 import { Typography } from '@mui/material'
-import type { NextPage } from 'next'
+import type { InferGetStaticPropsType, NextPage } from 'next'
+import Link from 'next/link'
+import styles from './index.module.css'
+import { getDatabase } from '@/domain/notion'
 
-const Posts: NextPage = () => {
-  return <Typography variant="h1">POSTS</Typography>
+const Posts: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ posts }) => {
+  return (
+    <>
+      <Typography variant="h1">POSTS</Typography>
+      <ol className={styles.posts}>
+        {posts.map((post) => {
+          const date = new Date(post.last_edited_time).toLocaleString('en-US', {
+            month: 'short',
+            day: '2-digit',
+            year: 'numeric',
+          })
+          console.log(post.properties)
+          return (
+            <li key={post.id} className={styles.post}>
+              <h3 className={styles.postTitle}>
+                <Link href={`/posts/${post.id}`}>
+                  <a>
+                    {/* @ts-ignore title の型が取得できない */}
+                    <Typography>{post.properties.Name.title[0].plain_text}</Typography>
+                  </a>
+                </Link>
+              </h3>
+
+              <p className={styles.postDescription}>{date}</p>
+              <Link href={`/posts/${post.id}`}>
+                <a> Read post →</a>
+              </Link>
+            </li>
+          )
+        })}
+      </ol>
+    </>
+  )
+}
+
+export const getStaticProps = async () => {
+  const database = await getDatabase()
+
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 1,
+  }
 }
 
 export default Posts
